0f4b073dc8d6eaf9774e40b378efcfeaa7140190
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-04-03 11:29:18 +0700

    final changes

--- Latihan2/Latihan2.cpp
+++ Latihan2/Latihan2.cpp
@@ -1,20 +1,39 @@
-// Latihan2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct AlamatDetail
 {
-    std::cout << "Hello World!\n";
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{
+    string NIM;
+    string nama;
+    AlamatDetail alamat;
+    int umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+
+    cout << "Masukan NIM :";
+    cin >> mhs.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs.nama;
+    cout << "Alamat :";
+    cout << "\n\tMasukan Desa :";
+    cin >> mhs.alamat.desa;
+    cout << "\tMasukan Kota :";
+    cin >> mhs.alamat.kota;
+    cout << "Masukan Umur :";
+    cin >> mhs.umur;
+
+    cout << "\n\nNIM = " << mhs.NIM;
+    cout << "\nNama = " << mhs.nama;
+    cout << "\nDesa = " << mhs.alamat.desa;
+    cout << "\nkota = " << mhs.alamat.kota;
+    cout << "\nUmur = " << mhs.umur;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
