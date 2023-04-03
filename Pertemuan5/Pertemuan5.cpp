01531d64909c0d602a8231353ce0dc95c2d82495
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-04-03 10:11:10 +0700

    Add project files.

---
+++ Pertemuan5/Pertemuan5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    mahasiswa mhs1, mhs2
+}
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
