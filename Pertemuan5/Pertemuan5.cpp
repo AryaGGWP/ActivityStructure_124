de28cfac2dbcb19fa3367f24ef15959f3cde2e36
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-04-03 10:31:00 +0700

    structure mahasiswa

--- Pertemuan5/Pertemuan5.cpp
+++ Pertemuan5/Pertemuan5.cpp
@@ -12,12 +12,30 @@ struct mahasiswa
 int main() {
     mahasiswa mhs1, mhs2;
 
-    mhs1.NIM = "20210140019";
-    mhs1.nama = "Ardian Mulya";
-    mhs1.alamat = "Wonosobo";
-    mhs1.umur = 20;
-}
+    mhs1.NIM = "20220140124";
+    mhs1.nama = "Arya GG";
+    mhs1.alamat = "Lampung";
+    mhs1.umur = 19;
 
+    cout << "Masukan NIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs2.nama;
+    cout << "Masukan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukan Umur :";
+    cin >> mhs2.umur;
+
+    cout << "\nNIM = " << mhs1.NIM;
+    cout << "\nNama = " << mhs1.nama;
+    cout << "\nAlamat = " << mhs1.alamat;
+    cout << "\nUmur = " << mhs1.umur;
+
+    cout << "\n\nNIM = " << mhs2.NIM;
+    cout << "\nNama = " << mhs2.nama;
+    cout << "\nAlamat = " << mhs2.alamat;
+    cout << "\nUmur = " << mhs2.umur;
+}
 
 
 
